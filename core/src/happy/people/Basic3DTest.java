6616da370f3d4667af56f400372497f11b21f2b7
Trevor Edwards <tre23@cornell.edu>
2017-04-01 21:13:27 -0400

    annotated

--- core/src/happy/people/Basic3DTest.java
+++ core/src/happy/people/Basic3DTest.java
@@ -18,7 +18,7 @@ import com.badlogic.gdx.math.Vector3;
 
 import java.util.ArrayList;
 
-public class Basic3DTest implements ApplicationListener, RendererForVR {
+public class Basic3DTest implements ApplicationListener {
 
     public Environment environment;
     public PerspectiveCamera cam;
@@ -38,10 +38,9 @@ public class Basic3DTest implements ApplicationListener, RendererForVR {
     static final float OBSTACLE_HEIGHT = 2f;
     static final float OBSTACLE_DEPTH = 0.9f;
     static final float SPEED_ADD = 0.0004f;
+    static final float BASE_SPEED = 0.4f;
     float baseSpeed;
     float velocity = 0.0f;
-    public VRCamera camv;
-    VRCameraInputAdapter ugh;
     BitmapFont font; //or use alex answer to use custom font
     boolean gameOn;
     MuseOscServer mos;
@@ -62,36 +61,39 @@ public class Basic3DTest implements ApplicationListener, RendererForVR {
     }
 
     public void reset() {
-        baseSpeed = 0.4f;
+        baseSpeed = BASE_SPEED;
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.15f, 0.1f, 0.1f, 9f));
         environment.add(new DirectionalLight().set(0.3f, 0.3f, 0.3f, 0f, 90f, 0f));
+
         modelBatch = new ModelBatch();
         batchBatch = new SpriteBatch();
         font = new BitmapFont();
 
         cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        camv = new VRCamera(67, 0.01f, 128f, 0.1f, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), this);
-        camv.update();
-        ugh = new VRCameraInputAdapter(camv);
         cam.position.set(0, 0, camheight);
         cam.lookAt(0,10000,0);
         cam.near = 1f;
         cam.far = 300f;
         cam.update();
+
         ModelBuilder modelBuilder = new ModelBuilder();
-        model = modelBuilder.createBox(5f, 5f, 50f,
-                new Material(ColorAttribute.createDiffuse(Color.BLUE)),
-                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+
+        // Fog
         Color fogColor = new Color(0.01f,0.01f,0.01f,0.97f);
         Model fogModel = modelBuilder.createBox(1000f,10f,1000f, new Material(ColorAttribute.createDiffuse(fogColor)), VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
         fogModel.materials.get(0).set(new BlendingAttribute(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA));
         fogInstance = new ModelInstance(fogModel);
         fogInstance.transform.setToTranslation(0,150f,0);
+
+        // Grass
         grass = modelBuilder.createBox(200f,200f,2f, new Material(ColorAttribute.createDiffuse(Color.DARK_GRAY)),VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
         grassInstance = new ModelInstance(grass);
-        // Water
 
+        // Skyscrapers
+        model = modelBuilder.createBox(5f, 5f, 50f,
+                new Material(ColorAttribute.createDiffuse(Color.BLUE)),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
         movingModels = new ArrayList<ModelInstance>();
         ModelInstance exInstance = new ModelInstance(model);
         exInstance.transform.setToTranslation(5,0,-1);
@@ -99,6 +101,7 @@ public class Basic3DTest implements ApplicationListener, RendererForVR {
         exInstance = new ModelInstance(model);
         exInstance.transform.setToTranslation(-5,25,-1);
         movingModels.add(exInstance);
+
         // Make sidewalk
         Model sidewalkModel = modelBuilder.createBox(3f, 4.9f, 1f,
                 new Material(ColorAttribute.createReflection(Color.TAN)),
@@ -109,7 +112,7 @@ public class Basic3DTest implements ApplicationListener, RendererForVR {
             movingModels.add(sInstance);
         }
 
-        //hurdles
+        // Hurdles
         hurdles = new ArrayList<ModelInstance>();
         Model hurdleModel = modelBuilder.createBox(3f, 0.9f, OBSTACLE_HEIGHT + 1,
                 new Material(ColorAttribute.createReflection(Color.YELLOW)),
@@ -125,13 +128,12 @@ public class Basic3DTest implements ApplicationListener, RendererForVR {
     @Override
     public void render () {
         float secondsElapsed = Gdx.graphics.getDeltaTime();
-       // ugh.update(secondsElapsed);
-       // camv.update();
         time++;
 
         if (gameOn) {
             speed = mos.getNormalizedStressVar() + baseSpeed;
             baseSpeed += SPEED_ADD;
+
             // Shitty physics
             float z = cam.position.z;
             if (z > camheight) {
@@ -225,55 +227,4 @@ public class Basic3DTest implements ApplicationListener, RendererForVR {
     public void pause () {
     }
 
-    @Override
-    public void renderForVR(PerspectiveCamera cam) {
-        float secondsElapsed = Gdx.graphics.getDeltaTime();
-        time++;
-
-        // Shitty physics
-        float z = cam.position.z;
-        if (z > camheight) {
-            // Flying
-            velocity -= gravity * secondsElapsed * speed;
-            z += velocity * speed;
-        } else {
-            // Not flying
-            z = camheight;
-            if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-                velocity = JUMP_VELOCITY;
-                z += 0.0001;
-            } else {
-                velocity = 0;
-            }
-        }
-        //cam.rotate(0.5f, 0,1,0);
-        if (time % 120 > 60) {
-            // cam.position.set(0, 0, camheight + (addheight * (1/60f) * (time % 60)) );
-        } else {
-            // cam.position.set(0, 0, camheight + addheight - (addheight * (1/60f) * (time % 60)) );
-        }
-        cam.position.z = z;
-        cam.update();
-        //instance.transform.setFromEulerAngles(0,0,time);
-        for (ModelInstance instance : movingModels) {
-            Vector3 give = new Vector3();
-            Vector3 mxyz = instance.transform.getTranslation(give);
-            mxyz.y -= speed * 0.3;
-            if (mxyz.y > resetDistance) mxyz.y = resetPos;
-            if (mxyz.y < resetPos) mxyz.y = resetDistance;
-            instance.transform.setToTranslation(mxyz.x,mxyz.y,mxyz.z);
-        }
-
-        grassInstance.transform.setToTranslation(0,10,-2);
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-
-        modelBatch.begin(cam);
-        modelBatch.render(grassInstance, environment);
-        for (ModelInstance instance : movingModels) {
-            modelBatch.render(instance, environment);
-        }
-        modelBatch.render(fogInstance,environment);
-        modelBatch.end();
-    }
 }
